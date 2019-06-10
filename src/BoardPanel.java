0232b0b4db5da701c2624c9fbd35fac1f701839a
Yana Krukovska <jana.krua@gmail.com>
2019-06-10 12:04:54 +0300

    Cards are now in array and are added to board panel in random order

--- src/BoardPanel.java
+++ src/BoardPanel.java
@@ -1,9 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 
 public class BoardPanel extends JPanel {
+
+
+    public Card[] cardsLevel1 = new Card[]{new Card("cake1.jpg"), new Card("cake2.jpg"),
+            new Card("cake1.jpg"), new Card("cake2.jpg")};
+
+    private int level1CardsAmount = 4;
+    private ImageIcon cardBack = new ImageIcon("C:\\IdeaProjects\\pairs-memory-game\\src\\images\\backCard.jpg");
+
 
 
     public BoardPanel() {
@@ -20,50 +30,37 @@ public class BoardPanel extends JPanel {
             setBounds(0, PairsGame.TOP_PANEL_HEIGHT + 2, PairsGame.FRAME_WIDTH, PairsGame.PANEL_HEIGHT);
 
             setLayout(new GridLayout(2, 2));
-            ImageIcon cardBack = new ImageIcon("C:\\IdeaProjects\\pairs-memory-game\\src\\images\\backCard.jpg");
-            ImageIcon level1Icon1 = new ImageIcon("C:\\IdeaProjects\\pairs-memory-game\\src\\images\\cake1.jpg");
-            ImageIcon level1Icon2 = new ImageIcon("C:\\IdeaProjects\\pairs-memory-game\\src\\images\\cake2.jpg");
-            ImageIcon level1Icon3 = new ImageIcon("C:\\IdeaProjects\\pairs-memory-game\\src\\images\\cake1.jpg");
-            ImageIcon level1Icon4 = new ImageIcon("C:\\IdeaProjects\\pairs-memory-game\\src\\images\\cake2.jpg");
-            Image image1 = level1Icon1.getImage();
-            Image image2 = level1Icon2.getImage();
-            Image image3 = level1Icon3.getImage();
-            Image image4 = level1Icon4.getImage();
-            level1Icon1 = new ImageIcon(image1);
-            level1Icon2 = new ImageIcon(image2);
-            level1Icon3 = new ImageIcon(image3);
-            level1Icon4 = new ImageIcon(image4);
-            JLabel level1Label1 = new JLabel(level1Icon1);
-            JLabel level1Label2 = new JLabel(level1Icon2);
-            JLabel level1Label3 = new JLabel(level1Icon3);
-            JLabel level1Label4 = new JLabel(level1Icon4);
-
             Timer showCardsTimer = new Timer(1500, new ActionListener() {
-
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    level1Label1.setIcon(cardBack);
-                    level1Label2.setIcon(cardBack);
-                    level1Label3.setIcon(cardBack);
-                    level1Label4.setIcon(cardBack);
+                    for (int i = 0; i < cardsLevel1.length; i++) {
+                        cardsLevel1[i].getImageLabel().setIcon(cardBack);
+                    }
+
                 }
             });
             showCardsTimer.setRepeats(false);
             showCardsTimer.start();
 
-
-            add(level1Label1);
-            add(level1Label2);
-            add(level1Label3);
-            add(level1Label4);
-
-            revalidate();
-
-
-
+            while (level1CardsAmount != 0)
+                addLevel1Cards();
 
         }
 
+
+        revalidate();
+
+    }
+
+
+    private void addLevel1Cards() {
+        Random generator = new Random();
+        int randomIndex = generator.nextInt(cardsLevel1.length);
+        if (!cardsLevel1[randomIndex].isOnBoard()) {
+            level1CardsAmount--;
+            cardsLevel1[randomIndex].setOnBoard(true);
+            add(cardsLevel1[randomIndex].getImageLabel());
+        }
     }
 
 }
