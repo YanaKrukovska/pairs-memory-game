7ce566c345f775228022146512677871a845b074
Yana Krukovska <jana.krua@gmail.com>
2019-06-12 20:51:04 +0300

    Changed menu panel to instance of separate MenuPanel class and removed methods and fields of menu panel there

--- src/PairsGame.java
+++ src/PairsGame.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -9,46 +8,34 @@ public class PairsGame extends JFrame {
     public final static int FRAME_HEIGHT = 500;
     public final static int TOP_PANEL_HEIGHT = FRAME_HEIGHT / 5;
     public final static int PANEL_HEIGHT = FRAME_HEIGHT - TOP_PANEL_HEIGHT;
-    public JButton okayButton;
-    private int levelCount = 0;
-
+    private final MenuPanel menuPanel;
+    private BoardPanel gameBoard;
 
     public PairsGame() {
 
         setTitle("Pairs");
-        setSize(FRAME_WIDTH + 15, FRAME_HEIGHT + 40);
-
+        setSize(FRAME_WIDTH + 8, FRAME_HEIGHT + 30);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
-
         setLayout(null);
-        JPanel menuPanel = new JPanel();
-        menuPanel.setLayout(null);
-        menuPanel.setBackground(Color.BLUE);
-        JTextField loginField = setStartComponents(menuPanel);
-        menuPanel.setBounds(0, 0, getWidth(), getHeight() / 5);
+
+        menuPanel = new MenuPanel();
+        menuPanel.setBounds(0, 0, FRAME_WIDTH, TOP_PANEL_HEIGHT);
 
 
-        BoardPanel gameBoard = new BoardPanel();
+        gameBoard = new BoardPanel(menuPanel);
         gameBoard.setBounds(0, TOP_PANEL_HEIGHT, FRAME_WIDTH, PANEL_HEIGHT);
 
-        okayButton.addActionListener(new ActionListener() {
+        menuPanel.getOkayButton().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (e.getSource() == okayButton) {
-                    String username = loginField.getText();
+                if (e.getSource() == menuPanel.getOkayButton()) {
 
-                    menuPanel.removeAll();
-                    menuPanel.repaint();
-                    JLabel usernameLabel = new JLabel("User: " + username);
-                    JLabel level = new JLabel("Level: " + ++levelCount);
-                    usernameLabel.setFont(new Font("Serif", Font.BOLD, 16));
-                    level.setFont(new Font("Serif", Font.BOLD, 16));
+                        menuPanel.removeAll();
+                        menuPanel.repaint();
+                        menuPanel.generateMenu(5);
+                        gameBoard.updateBoard(5);
 
-                    menuPanel.add(usernameLabel).setBounds(10, 20, 100, 20);
-                    menuPanel.add(level).setBounds(110, 20, 100, 20);
-                    menuPanel.revalidate();
-                    gameBoard.updateBoard(levelCount);
                     gameBoard.setBounds(0, TOP_PANEL_HEIGHT, FRAME_WIDTH, PANEL_HEIGHT);
                 }
             }
@@ -60,26 +47,6 @@ public class PairsGame extends JFrame {
         setVisible(true);
     }
 
-    private JTextField setStartComponents(JPanel menuPanel) {
-        JLabel welcomeMessage = new JLabel("Welcome to Pairs!");
-        welcomeMessage.setFont(new Font("Serif", Font.BOLD, 20));
-
-        JLabel loginLabel = new JLabel("Login");
-        JTextField loginField = new JTextField();
-        okayButton = new JButton("Ok");
-
-        welcomeMessage.setBounds(325, 15, 200, 20);
-        loginLabel.setFont(new Font("Serif", Font.BOLD, 16));
-        loginLabel.setBounds(375, 45, 100, 20);
-        loginField.setBounds(350, 65, 100, 20);
-        okayButton.setBounds(350, 80, 100, 20);
-
-        menuPanel.add(welcomeMessage);
-        menuPanel.add(loginLabel);
-        menuPanel.add(loginField);
-        menuPanel.add(okayButton);
-        return loginField;
-    }
 
 
 }
