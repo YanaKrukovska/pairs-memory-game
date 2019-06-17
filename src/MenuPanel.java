806bbba8b55421def2928a96f69841d408f64ef6
Mariesnlk <44849051+Mariesnlk@users.noreply.github.com>
2019-06-17 11:02:04 +0300

    Update MenuPanel.java
    add background for menu, button dound

--- src/MenuPanel.java
+++ src/MenuPanel.java
@@ -1,5 +1,13 @@
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
 public class MenuPanel extends JPanel {
 
@@ -8,6 +16,9 @@ public class MenuPanel extends JPanel {
     private JLabel levelLabel;
     private String username;
 
+    Image fon = new ImageIcon("C:\\Users\\Марія\\Documents\\Лабораторна_Синельник\\Memory Game\\fon.jpg").getImage();
+
+
     public MenuPanel() {
         setLayout(null);
         loginField = setStartComponents();
@@ -15,13 +26,21 @@ public class MenuPanel extends JPanel {
 
     private JTextField setStartComponents() {
         JLabel welcomeMessage = new JLabel("Welcome to Pairs!");
-        welcomeMessage.setFont(new Font("Serif", Font.BOLD, 20));
+        welcomeMessage.setFont(new Font("Serif", Font.BOLD, 30));
+        welcomeMessage.setForeground(Color.BLUE);
 
         JLabel loginLabel = new JLabel("Login");
         JTextField loginField = new JTextField();
         okayButton = new JButton("Ok");
 
-        welcomeMessage.setBounds(325, 15, 200, 20);
+        JButton buttonSound = new JButton("S");
+        buttonSound.setBounds(700, 10, 50, 30);
+        buttonSound.setFocusable(false);
+        ActionListener actionListener = new ButtonPushActionListener();
+        buttonSound.addActionListener(actionListener);
+
+
+        welcomeMessage.setBounds(300, 15, 400, 20);
         loginLabel.setFont(new Font("Serif", Font.BOLD, 16));
         loginLabel.setBounds(375, 45, 100, 20);
         loginField.setBounds(350, 65, 100, 20);
@@ -30,8 +49,40 @@ public class MenuPanel extends JPanel {
         add(welcomeMessage);
         add(loginLabel);
         add(loginField);
-        add(okayButton);
+        add(buttonSound);
         return loginField;
+    }
+
+    private class ButtonPushActionListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            try
+            {
+                File soundFile = new File("C:\\Users\\Марія\\Documents\\Лабораторна_Синельник\\Memory Game\\sounds.wav");
+                AudioInputStream inAudio = AudioSystem.getAudioInputStream(soundFile);
+                Clip clip = AudioSystem.getClip();
+                clip.open(inAudio);
+                clip.setFramePosition(0);
+
+                FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+                gainControl.setValue((float) -20.0);
+                clip.start();
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
+            }
+            catch (Exception e1)
+            {
+                System.out.println("You have a problem with audio file!!");
+            }
+        }
+    }
+
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if(fon != null){
+            g.drawImage(fon, 0, 0, getWidth(), getHeight(), null);
+        }
     }
 
 
@@ -76,3 +127,4 @@ public class MenuPanel extends JPanel {
         revalidate();
     }
 }
+
